6484658397b5e642ca7664737db24d711a27f3a9
AHMED SULTAN <95858731+ahmad199617@users.noreply.github.com>
2024-05-27 00:14:03 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -268,9 +268,11 @@ const animationTimeline = () => {
         0.3
     )
     .to(".six", 0.5, {
-        opacity: 0,
-        y: 30,
-        zIndex: "-1",
+
+
+
+
+
     })
     .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
     .to(
